26d490c4c43c65a323a5f0217fb6aab910db6315
Peter Darinzo <birel44@yahoo.com>
2022-03-07 17:55:03 -0600

    complete binary tree

--- binary-tree.js
+++ binary-tree.js
@@ -6,6 +6,10 @@ class BinaryTreeNode {
     this.left = left;
     this.right = right;
   }
+
+  isLeaf() {
+    return ((this.left === null) && (this.right === null));
+  }
 }
 
 class BinaryTree {
@@ -17,20 +21,71 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
-
+    if (this.root === null) return 0; // handle empty tree
+    let depth = 1;
+    let traversed = 0;
+    let toVisitQueue = [this.root];
+    while (toVisitQueue.length) {
+      let current = toVisitQueue.shift();
+      traversed++;
+      if (Math.pow(2, depth) === traversed) depth++; // if 2^depth nodes have been searched, increment depthf
+      if (current.isLeaf()) return depth; // the first leaf found is the min depth
+      if (current.left) toVisitQueue.push(current.left);
+      if (current.right) toVisitQueue.push(current.right);
+    }
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
-
+    if (this.root === null) return 0; // handle empty tree
+    let depth = 0;
+    let toVisitQueue = [this.root];
+    while (toVisitQueue.length) {
+      depth++;
+      for (let i = 0; i < toVisitQueue.length; i++) {
+        let node = toVisitQueue.shift();
+        if (node.left) toVisitQueue.push(node.left);
+        if (node.right) toVisitQueue.push(node.right);
+      }
+    }
+    return depth;
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let maxSum = 0;
+    if (this.root === null) return 0; // handle empty tree
+
+    function isLeaf(node) {
+      return ((node.left === null) && (node.right === null));
+    }
+
+    function findMax(node) {
+      if (isLeaf(node)) {
+        return node.val;
+      } else {
+        let leftMax = 0;
+        let rightMax = 0;
+        if (node.left) {
+          leftMax = findMax(node.left);
+        }
+        if (node.right) {
+          rightMax = findMax(node.right);
+        }
+        const childMax = Math.max(leftMax, rightMax);
+        const childRootMax = Math.max(node.val, childMax + node.val);
+        const pathMax = Math.max(childRootMax, node.val + leftMax + rightMax);
+        maxSum = Math.max(maxSum, pathMax);
+        return childRootMax;
+      }
+    }
+
+    findMax(this.root);
+    return maxSum;
 
   }
 
@@ -38,7 +93,20 @@ class BinaryTree {
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
-
+    if (this.root === null) return null; // handle empty tree
+    let min = null;
+    let toVisitQueue = [this.root];
+    while (toVisitQueue.length) {
+      let current = toVisitQueue.shift();
+      if (current.val > lowerBound) {
+        if (!min || (min > current.val)) {
+          min = current.val;
+        }
+      }
+      if (current.left) toVisitQueue.push(current.left);
+      if (current.right) toVisitQueue.push(current.right);
+    }
+    return min;
   }
 
   /** Further study!
@@ -68,7 +136,7 @@ class BinaryTree {
    * of two nodes in a binary tree. */
 
   lowestCommonAncestor(node1, node2) {
-    
+
   }
 }
 
